2714ce81851fccec5ce08139a21a4afe7b3ff655
David Mckay <davidmckay4225@gmail.com>
2026-08-20 22:19:16 +0100

    setup empty react project

---
+++ code/frontend/biddingfloor/src/App.tsx
@@ -0,0 +1,23 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from './assets/vite.svg'
+//import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+	//const hello = 1;
+
+  return (
+  <div>
+        <h1>Bidding Floor</h1>
+        
+	<h2>Bids</h2>
+	<h2>listings</h2>
+	<h2>Alerts</h2>
+	<h2>Account</h2>
+      </div>
+  )
+}
+
+export default App
+
