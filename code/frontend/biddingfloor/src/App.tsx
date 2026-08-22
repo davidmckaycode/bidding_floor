b4994c0462f28e00f52651f625347c6af71888dc
David Mckay <davidmckay4225@gmail.com>
2026-08-22 14:24:33 +0100

    added basic ui layout

--- code/frontend/biddingfloor/src/App.tsx
+++ code/frontend/biddingfloor/src/App.tsx
@@ -4,20 +4,41 @@
 //import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-	//const hello = 1;
+
+
+
+function Navigation_Bar() {
 
   return (
-  <div>
-        <h1>Bidding Floor</h1>
-        
-	<h2>Bids</h2>
-	<h2>listings</h2>
-	<h2>Alerts</h2>
-	<h2>Account</h2>
-      </div>
+    <nav className={`order-first flex flex-col h-screen border-r-1`}>
+
+		<h1 className="border-b-1">Bidding Floor</h1>
+		<ul className="flex flex-col flex-1">
+			<li>Search</li>
+			<li>Bids</li>
+			<li>listings</li>
+			<li>Alerts</li>
+			<li className="mt-auto border-t-1">Account</li>
+		</ul>
+	</nav>
   )
 }
 
+
+function App() {
+
+  return (
+  	<div className="flex">
+  		<Navigation_Bar/>
+  	        <h1 className="text-center flex-1 border-b-1 self-start">PageInfo</h1>
+        </div>
+  )
+
+
+
+
+}
 export default App
+
+
 
