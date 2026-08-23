e108a7e19c9d911e9408059fe4b7cc072f5376cc
David Mckay <davidmckay4225@gmail.com>
2026-08-23 15:16:34 +0100

    created basic ui

--- code/frontend/biddingfloor/src/App.tsx
+++ code/frontend/biddingfloor/src/App.tsx
@@ -1,39 +1,59 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from './assets/vite.svg'
-//import heroImg from './assets/hero.png'
 import './App.css'
 
 
-
-
-function Navigation_Bar() {
+function Content_Area() {
 
   return (
-    <nav className={`order-first flex flex-col h-screen border-r-1`}>
-
-		<h1 className="border-b-1">Bidding Floor</h1>
-		<ul className="flex flex-col flex-1">
-			<li>Search</li>
-			<li>Bids</li>
-			<li>listings</li>
-			<li>Alerts</li>
-			<li className="mt-auto border-t-1">Account</li>
-		</ul>
-	</nav>
+	<div className="flex-1 w-full rounded-lg mt-[1vw] bg-[var(--floating-ui-background-color)]"></div>
   )
 }
 
 
-function App() {
+function TopBar() {
+  return (
+  	<div className="flex items-start justify-center">
+  		<h1 className="text-center rounded-lg w-1/4 bg-[var(--floating-ui-background-color)]">PageInfo</h1>
+  	</div>
+  )
+}
+
+function Side_Bar() {
 
   return (
-  	<div className="flex">
-  		<Navigation_Bar/>
-  	        <h1 className="text-center flex-1 border-b-1 self-start">PageInfo</h1>
+    <nav className="order-first flex h-full flex-col rounded-lg shrink-0 bg-[var(--floating-ui-background-color)]">
+		<h1 className="whitespace-nowrap">Bidding Floor</h1>
+		<ul className="flex flex-col flex-1 text-[#4d4d4d] [&_*]:text-sm divide-y">
+			<li>Search</li>
+			<li>Bids</li>
+			<li>listings</li>
+			<li>Alerts</li>
+		</ul>
+		<h1 className="mt-auto border-t-1">Account</h1>
+	</nav>
+  )
+}
+
+function AppContent() {
+  return (
+
+	<div className="h-screen w-screen p-[1vw] bg-[var(--app-background-color)]">
+        	<div className="flex flex-1 flex-row w-full h-full">
+        		<div><Side_Bar/></div>
+        		<div className="flex flex-col flex-1 pl-[1vw] bg-[var(--app-background-color)]">
+        			<TopBar/>
+        			<Content_Area/>
+        		</div>
+        	</div>
         </div>
   )
+}
 
+
+
+function App() {
+  return (
+  	<AppContent/>
+  )
 
 
 
